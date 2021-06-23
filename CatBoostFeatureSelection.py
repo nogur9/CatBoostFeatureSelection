148878a686f6c49a296bd3484cddebc1ea800a79
nogur9 <nogur9@gmail.com>
2021-06-23 15:59:17 +0300

    Add files via upload

---
+++ CatBoostFeatureSelection.py
@@ -0,0 +1,29 @@
+from catboost import CatBoostClassifier
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class CatBoostFeatureSelection(BaseEstimator, TransformerMixin):
+
+    def __init__(self, num_features_to_select=15, eval_set=None):
+
+        self.num_features_to_select = num_features_to_select
+        self.selected_features_indices = []
+        self.eval_set = eval_set
+
+    def transform(self, X, y=None):
+
+        _X = X.iloc[:, self.selected_features_indices]
+
+        return _X
+
+    def fit(self, X, y):
+        cb = CatBoostClassifier(verbose=False, boosting_type='Ordered', approx_on_full_history=True)
+
+        selected = cb.select_features(X, y, features_for_select= X.columns,
+                                      num_features_to_select=self.num_features_to_select,
+                                      verbose=False, eval_set=self.eval_set, logging_level='silent')
+
+        self.selected_features_indices = selected['selected_features']
+
+        return self
+
